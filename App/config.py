118268578c32905e57283c79fdd931a67184aca4
sergioa2 <s.arangoa@uniandes.edu.co>
2021-02-11 16:01:44 -0500

    .

--- App/config.py
+++ App/config.py
@@ -4,4 +4,4 @@ file_path = os.path.join(os.path.dirname(__file__), '..')
 file_dir = os.path.dirname(os.path.realpath('__file__'))
 sys.path.insert(0, os.path.abspath(file_path))
 data_dir = file_dir + '/Data/'
-'main'
+'prueba_sergio'
